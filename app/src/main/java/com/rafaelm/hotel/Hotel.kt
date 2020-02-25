51a86408a4c3f0363f4573efa6216fccc069e627
RafaelM <RafaelM.Science@gmail.com>
2020-02-25 07:21:03 -0400

    init commit
    funcionando os processos

---
+++ app/src/main/java/com/rafaelm/hotel/Hotel.kt
@@ -0,0 +1,10 @@
+package com.rafaelm.hotel
+
+class Hotel(
+    var id: Long = 0,
+    var name: String = "",
+    var address: String = "",
+    var rating: Float = 0.0f
+){
+    override fun toString(): String = name
+}
